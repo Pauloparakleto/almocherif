35df6ae5dd9faf3e5ce954a8afa35ef37da852fa
paulo-felipe <alparakleto@gmail.com>
2021-08-04 19:47:37 -0300

    feat(items_controller.rb): add entry and exit actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,15 +39,25 @@ class ItemsController < ApplicationController
     if @item.destroy
       redirect_to items_path
     else
-       redirect_back fallback_location: items_path
+      redirect_back fallback_location: items_path
     end
+  end
+
+  def entry
+    @item = Item.find(params[:id])
+    StockRegister.new(item: @item, options: params[:options]).entry
+  end
+
+  def exit
+    @item = Item.find(params[:id])
+    StockRegister.new(item: @item, options: params[:options]).exit
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :quantity)
-    end
+  end
 
   def item_params_update
     params.require(:item).permit(:name)
