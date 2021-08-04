e9530d9d1cc07544561d6819276f91dabc663d96
paulo-felipe <alparakleto@gmail.com>
2021-08-04 12:01:34 -0300

    test(models/item_spec.rb): add crud test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Item, type: :model do
     item = FactoryBot.create(:item)
     expect(item).to be_truthy
   end
+
+  it 'should delete' do
+    item = FactoryBot.create_list(:item, 10)
+    item.second.delete
+    expect(item.count).to eq(9)
+  end
+
+  it 'should create list' do
+    item = FactoryBot.create_list(:item, 10)
+    expect(item.count).to eq(10)
+  end
+
+  it 'update quantity' do
+    item = FactoryBot.create(:item)
+
+    quantity = item.quantity
+    item.update(quantity: 20)
+
+    expect(item.quantity).to eq(20)
+    expect(quantity).not_to eq(item.quantity)
+  end
 end
