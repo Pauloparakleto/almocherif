88a2525515e1112ada06c2e51dc19653d9dcc52a
paulo-felipe <alparakleto@gmail.com>
2021-08-04 19:45:10 -0300

    refactor(business/stock_register.rb): add entry and exit  params version

--- spec/business/stock_register_spec.rb
+++ spec/business/stock_register_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 RSpec.describe StockRegister do
   it 'should entry' do
     item = FactoryBot.create(:item, quantity: 0)
-    result = StockRegister.new(item: item, options: 2).entry
+    params = { item: item, options: 2 }
+    result = StockRegister.new(params).entry
 
     expect(result.quantity).to eq(2)
   end
 
   it 'should exit' do
     item = FactoryBot.create(:item, quantity: 2)
-    result = StockRegister.new(item: item, options: 2).exit
+    params = { item: item, options: 2 }
+    result = StockRegister.new(params).exit
 
     expect(result.quantity).to eq(0)
   end
