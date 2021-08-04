0076c16e7f00eb2073d9c0373932e2ba8d37d70f
paulo-felipe <alparakleto@gmail.com>
2021-08-04 19:47:06 -0300

    feat(routes.rb): add entry and exit post requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :items do
+    collection do
+      post :entry
+      post :exit
+    end
+  end
 end
