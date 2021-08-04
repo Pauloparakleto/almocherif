2f116c0f08a1702c672eadce729b04a44da6a0c3
paulo-felipe <alparakleto@gmail.com>
2021-08-04 12:05:58 -0300

    feat(items_controller.rb): add crud actions

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,51 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.valid?
+      redirect_to @item
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path @item
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to items_path
+    else
+       redirect_back fallback_location: items_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :quantity)
+  end
+end
