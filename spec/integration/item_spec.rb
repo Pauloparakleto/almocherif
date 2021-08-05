a9d093b0d8414305cec31eb99126b65e605f1046
paulo-felipe <alparakleto@gmail.com>
2021-08-05 15:05:39 -0300

    fix(models/item_spec.rb): change response status to a redirect one

--- spec/integration/item_spec.rb
+++ spec/integration/item_spec.rb
@@ -52,6 +52,6 @@ RSpec.describe "Item", type: :request do
   it 'exit' do
     item = FactoryBot.create(:item, quantity: 2)
     post exit_items_path, params: { id: item.id, options: 2 }
-    expect(response.status).to eq(204)
+    expect(response.status).to eq(302)
   end
 end
