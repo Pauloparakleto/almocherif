a8851011094b5151fdbca5e7aebc28dfd79d702f
paulo-felipe <alparakleto@gmail.com>
2021-08-05 23:05:54 -0300

    feat(item.rb): add has one log

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,12 @@ class Item < ApplicationRecord
   before_destroy :can_destroy
   paginates_per 10
 
+  has_one :log
+
   def can_destroy
     if self.audited?
       errors.add(:base, :invalid, message: "Message here")
+      raise ActiveRecord::RecordInvalid
     end
   end
 end
