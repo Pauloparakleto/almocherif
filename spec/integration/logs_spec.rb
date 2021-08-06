75096b46aea2977ddffd45e22e751baea0b18f70
paulo-felipe <alparakleto@gmail.com>
2021-08-06 10:12:19 -0300

    refactor(integration/logs_spec.rb): include login via devise

--- spec/integration/logs_spec.rb
+++ spec/integration/logs_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Log", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+    sign_in @user
+  end
   it 'gets index' do
     get logs_path
     expect(response).to render_template("logs/index")
